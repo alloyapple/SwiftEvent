de100b25617178401d660bd29cac2b9212a38872
color <alloy.apple@gmail.com>
2019-04-08 12:04:57 +0800

    change header file

--- Sources/SwiftEvent/include/shim.h
+++ Sources/SwiftEvent/include/shim.h
@@ -6,10 +6,12 @@
 #define SWIFTEVENT_SHIM_H
 
 #include <sys/socket.h>
-#include <event2/bufferevent.h>
-#include <event2/buffer.h>
-#include <event2/listener.h>
-#include <event2/util.h>
-#include <event2/event.h>
+#include <event.h>
+#include <evhttp.h>
+#include <evutil.h>
+#include <evdns.h>
+
+
+
 
 #endif //SWIFTEVENT_SHIM_H
